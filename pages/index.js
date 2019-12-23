a1d34c734fa84f48100989563e260b363179d057
adkahane <adam.kahane.nj@gmail.com>
2019-12-22 22:02:31 -0500

    stuff

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import timeline from '../components/timeline';
+
 const Index = () => (
   <body>
     <div class="container">
@@ -32,11 +34,10 @@ const Index = () => (
           </tr>
         </tbody>
       </table>
-      <div id='timeline-embed' style={{width: '100%'}}></div>
+      <timeline />
     </div>
     {/* Optional JavaScript */}
     <script src="assets/app.js"></script>
-    <script src="https://cdn.knightlab.com/libs/timeline3/latest/js/timeline.js"></script>
     {/* jQuery first, then Popper.js, then Bootstrap JS */}
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
